036a69aec35632c19f0b785fe542669ca14163e3
Lukas Salficky <salficky.job@gmail.com>
2020-04-13 16:56:58 +0200

    Change gulp transpilation to webpack transpilation

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+﻿module.exports = {
+    mode: "production",
+    //mode: "development",
+    entry: {
+        app: './Typescript/main.tsx',
+        //search: './src/search.js'
+    },
+    output: {
+        filename: "[name].js",
+        path: __dirname + "/wwwroot/js/"
+    },
+    devtool: "source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+};
