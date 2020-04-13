8bd25d3c8265a0bbb721ee81737558a3ab12bba8
Lukas Salficky <salficky.job@gmail.com>
2020-04-13 17:33:29 +0200

    Create gulp task to run webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
 let gulp = require('gulp');
+const webpack_stream = require('webpack-stream');
+const webpack_config = require('./webpack.config.js');
+
+gulp.task('webpack', () => {
+    return webpack_stream(webpack_config)
+        .pipe(gulp.dest('./wwwroot/js/'));
+});
