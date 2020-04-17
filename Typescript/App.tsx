f401bfe9e7852ec41feb23bbce076cc5cdf08cf4
lukas4311 <lukas4311@gmail.com>
2020-04-17 19:37:36 +0200

    Tailwind base css. Add photo

--- Typescript/App.tsx
+++ Typescript/App.tsx
@@ -12,7 +12,12 @@ class AppComponent extends React.Component<{}, {}> {
             <div className="flex">
                 <div className="w-1/3">
                     <div className="flex">
-                        <LeftMenuComponent />
+                        <div className="w-2/10">
+                            <LeftMenuComponent />
+                        </div>
+                        <div className="w-8/10">
+                            <img src='./images/photo.jpg' />
+                        </div>
                     </div>
                 </div>
                 <div className="w-2/3">
