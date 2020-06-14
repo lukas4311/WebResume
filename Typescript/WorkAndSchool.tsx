f50778a12db58eb80806c79cc6ff3da3cea3f0ec
lukas4311 <lukas4311@gmail.com>
2020-06-14 19:32:03 +0200

    New texts work.

--- Typescript/WorkAndSchool.tsx
+++ Typescript/WorkAndSchool.tsx
@@ -7,8 +7,8 @@ const erzasoftWork: IExperienceCardProps = {
     Description: "Programování a kódování webových stránek za pomoci jazyka C# a technologií ASP.NET MVC 5.",
     DescriptionHtml:
         <p>
-            V Erasoftu jsme se věnovi vývoji a kódování webových stránek za pomoci jazyka C# a technologií ASP.NET MVC 5.
-            Mimo tyto technologie jsem používal také HTML5, Javascript, Sass pro kódování stránek.
+            V Erasoftu jsme se věnovi vývoji a kódování webových stránek na múru za pomoci jazyka C# a technologie ASP.NET MVC 5.
+            Mimo tyto technologie jsem také běžne používal také HTML5, Javascript, Sass pro kódování stránek.
         </p>,
     From: "05/2013",
     To: "03/2014",
@@ -18,11 +18,11 @@ const erzasoftWork: IExperienceCardProps = {
 };
 const trsWork: IExperienceCardProps = {
     Description: "Programování Windows Forms aplikací za pomoci jazyka C#",
-    DescriptionHtml: <p>Programování Windows Forms aplikací za pomoci jazyka C#</p>,
+    DescriptionHtml: <p>Zde jsem se věnoval vývoji Windows Forms aplikace za pomoci jazyka C#.</p>,
     From: "03/2014",
     To: "12/2014",
     Name: "TRS s.r.o.",
-    Technologies: ["EntityFramework", "MS SQL", "Telerik (UI for ASP.NET MVC)", "Javascript", "HTML5", "CSS3", "Sass", "TFS", "Adobe Photoshop"],
+    Technologies: ["Windows Forms"],
     Position: "Software developer"
 };
 const proficreditSoftwareDeveloperWork: IExperienceCardProps = {
@@ -32,7 +32,10 @@ const proficreditSoftwareDeveloperWork: IExperienceCardProps = {
             <p>
                 Jako software developer jsem se podílel na vývoji různých typů aplikací na .NET platformě.
                 Primárně se jednalo o aplikace vytvořené pomocí technologie ASP NET MVC 5, WPF a WCF. K verzování zdrojového kódu jsem využívali TFS.
-                Aplikace spolupracovaly s MSSQL databází.
+                Data jsem ukládali do MSSSQL databáze. Můj první větší projekt byl klientský portál, na kterém jsem dělal vyvíjel jsem ho pomoci technologie ASP .NET MVC 5.
+                Tento projekt jsem před svým odchodem přepisoval do .NET Core. Také jsme se podílel na vývoji našeho SOAP API pomocí WCF technologie. Mimo to se náš tým věnoval
+                vývoji REST API za pomoci ASP NET Core WebAPI, což byl také první .NET Core projekt v naší firmě, který se produkčně nasadil. Mimo to jsem se věnoval vývoji modulů
+                do našeho interního systému napsaného v WPF techonologii a MVVM patternu.
             </p>
         </div>,
     From: "01/2015",
@@ -47,9 +50,12 @@ const proficreditDevelopmentLeaderWork: IExperienceCardProps = {
         <div>
             <p>
                 S novou pozicí přišla nová zodpovědnost. Po rozšíření IT oddělení a rozdělení na malé teamy jsem se stal development leaderem jednoho z nich.
-                I s novou pozicí jsem se primárně věnoval programování projektů, které jsem v teamu měli na starost. Vyvíjeli jsme primárně ASP NET Core MVC, ASP NET Core WebApi a WPF.
-                Mimo to jsem se začal věnovat code review pro programátory v našem teamu a jako developement leader jsem měl možnost rozhodovat o architektuře aplikace a použité technologii.
-                Také jsem se více zapojoval co vývoje našich core projektů a diskuze o těchto projektech. V našem teamu probíhal vývoj plně agilně.
+                I s novou pozicí jsem se primárně věnoval programování projektů, které jsem v teamu měli na starost. Vyvíjeli jsme primárně ASP NET Core MVC, 
+                ASP NET Core WebApi a WPF. Pod ruma nám prošla spousta velých jako nové REST API pomocí technologie ASP .NET Core WebAPI nové moduly pro náš interní systém
+                nebo přepis klientského portálu do .NET Core. Mimo to jsem se začal věnovat code review pro programátory v našem teamu a snažil jsem se hlídat, aby kód který odpoštěl náš 
+                tým byl čistý a spolňoval SOLID principy. Jako developement leader jsem měl možnost rozhodovat o architektuře aplikace a použité technologii. 
+                Také jsem se více zapojoval do vývoje našich core projektů a diskuze o těchto projektech. V našem teamu probíhal vývoj plně agilně a k plánování sprintů 
+                jsme používali JIRU.
             </p>
         </div>,
     From: "01/2017",
